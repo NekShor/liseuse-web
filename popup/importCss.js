d3f4ddb62150de7c44d669af5dc9643b821eaff6
Antoine Bigot <62606118+NekShor@users.noreply.github.com>
2024-01-15 00:21:40 +0100

    v1

--- popup/importCss.js
+++ popup/importCss.js
@@ -1,13 +1,29 @@
+console.log(color1)
+console.log(color2)
 var css = `
-.liseuse{
-    color: red;
+.liseuse *{
+    background: ${color1}!important;
+    color: ${color2}!important;
+    background-image: none!important;
+    text-shadow: none!important;
+    box-shadow: none!important;
+    font-family: sans-serif!important;
+    font-size: ${taille}px!important;
+    font-weight: ${weight}!important;
+
 }
+
 `
 var style = document.createElement('style');
 
 if (style.styleSheet) {
     style.styleSheet.cssText = css;
 } else {
+    if(document.getElementById('liseusecss')){
+        document.getElementById('liseusecss').remove()
+    }
+
+    style.setAttribute('id', 'liseusecss');
     style.appendChild(document.createTextNode(css));
 }
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,48 +1,123 @@
-document.getElementById('input').addEventListener('change', setCss);
+document.getElementById('input').addEventListener('change', changeValue);
+document.getElementById('color1').addEventListener('change', changeValue);
+document.getElementById('color2').addEventListener('change', changeValue);
+document.getElementById('transparence').addEventListener('change', changeValue);
+document.getElementById('taille').addEventListener('change', changeValue);
+document.getElementById('weight').addEventListener('change', changeValue);
+
+
+function changeValue(){
+    var json = {
+        'inputChecked': document.getElementById('input').checked,
+        'color1': document.getElementById('color1').value,
+        'color2': document.getElementById('color2').value,
+        'transparence': document.getElementById('transparence').value,
+        'taille': document.getElementById('taille').value,
+        'weight': document.getElementById('weight').checked? 'bold' : 'normal'
+    }
+
+    chrome.storage.sync.set(json, function() {
+    });
+
+    setCss()
+    importCss()
+}
 
 function setCss(){
-    var input = document.getElementById('input').value
-
-    console.log(input)
-    var value = false
-    if(input === "on"){
-        value = true
-    }
-    console.log(value)
-
-    chrome.storage.sync.set({ 'inputChecked': value }, function() {
-    });
-
     chrome.storage.sync.get('inputChecked', function(data) {
-        console.log(data)
+        if(data.inputChecked){
+            chrome.tabs.executeScript({
+                code: `var body = document.querySelector('body')
+        
+                    if(!body.classList.contains('liseuse')){
+                        body.classList.add('liseuse')
+                    }
+                `
+            });
+        }else{
+            chrome.tabs.executeScript({
+                code: `var body = document.querySelector('body')
+        
+                    if(body.classList.contains('liseuse')){
+                        body.classList.remove('liseuse')
+                    }
+                `
+            });
+        }
     });
+}
 
-    if(input){
+
+function importCss(){
+    ( async () => {
+        var color1;
+        var color2;
+        var transparence;
+        
+        var newPremise = new Promise((resolve, reject) => {
+            chrome.storage.sync.get(['color1', 'color2', 'transparence', 'taille', 'weight'], function(data) {
+                if(data.color1 == undefined){
+                    data.color1 = '#0a0908'
+                }
+                if(data.color2 == undefined){
+                    data.color2 = '#e5e5e5'
+                }
+                if(data.transparence == undefined){
+                    data.transparence = '0.5'
+                }
+                if(data.taille == undefined){
+                    data.taille = '1'
+                }
+                if(data.weight == undefined){
+                    data.weight = 'normal'
+                }
+                resolve(data)
+            });
+            
+        })
+
+        var data = await newPremise;
+
+        
+
+        transparence = Math.round((data.transparence*255)/100).toString(16)
+
+        var colorTransparence = data.color1+transparence
+
         chrome.tabs.executeScript({
-            code: `var body = document.querySelector('body')
-
-            if(!body.classList.contains('liseuse')){
-                body.classList.add('liseuse')
-            }`
+            code: `
+                var color1 = '${colorTransparence}';
+                var color2 = '${data.color2}';
+                var taille = '${data.taille}';
+                var weight = '${data.weight}';
+            `
+        }, function() {
+            // Exécuter le script importé
+            chrome.tabs.executeScript({
+                file: './popup/importCss.js'
+            });
         });
-    }else{
-        chrome.tabs.executeScript({
-            code: `var body = document.querySelector('body')
+        
+    })();
 
-            if(body.classList.contains('liseuse')){
-                body.classList.remove('liseuse')
-            }`
-        });
-    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    chrome.tabs.executeScript({
-        file: './popup/importCss.js'
+    chrome.storage.sync.get(['color1', 'color2', 'transparence', 'taille', 'weight'], function(data) {
+        document.getElementById('color1').value = data.color1
+        document.getElementById('color2').value = data.color2
+        document.getElementById('transparence').value = data.transparence
+        document.getElementById('taille').value = data.taille
+        document.getElementById('weight').value = data.weight
     });
-    setCss()
 
+    importCss()
+    
     chrome.storage.sync.get('inputChecked', function(data) {
+        console.log(data)
         document.getElementById('input').checked = data.inputChecked;
     });
+
+    setCss()
 })
+
